0c33a22a75d0a47dc4f18a9158048e633eea5a71
manuzagra <manuzagra@gmail.com>
2020-04-16 02:45:08 +0200

    working nicely

--- src/main.cpp
+++ src/main.cpp
@@ -48,13 +48,14 @@ int main() {
   }
 
   //////////////////////////////////////////////////
-  double vel = 0.0;
-  double acc = 0.224;
+  // initial lane
   int lane = 1;
+  // initial speed
+  double vel = 0.0;
   //////////////////////////////////////////////////
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy,&lane,&vel,&acc]
+               &map_waypoints_dx,&map_waypoints_dy,&lane,&vel]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -77,15 +78,15 @@ int main() {
           double car_y = j[1]["y"];
           double car_s = j[1]["s"];
           double car_d = j[1]["d"];
-          double car_yaw = j[1]["yaw"];
+          double car_yaw = deg2rad(j[1]["yaw"]);
           double car_speed = j[1]["speed"];
 
           // Previous path data given to the Planner
-          auto previous_path_x = j[1]["previous_path_x"];
-          auto previous_path_y = j[1]["previous_path_y"];
+          auto previous_path_x_ = j[1]["previous_path_x"];
+          auto previous_path_y_ = j[1]["previous_path_y"];
           // Previous path's end s and d values
-          double end_path_s = j[1]["end_path_s"];
-          double end_path_d = j[1]["end_path_d"];
+          double end_path_s_ = j[1]["end_path_s"];
+          double end_path_d_ = j[1]["end_path_d"];
 
           // Sensor Fusion Data, a list of all other cars on the same side
           //   of the road.
@@ -101,21 +102,48 @@ int main() {
            *   sequentially every .02 seconds
            */
 
-           // we do this to continue with the last path
-           unsigned int prev_size = previous_path_x.size();
+           // constants
+           float lane_width = 4;
+           float max_speed = 49;
+           float dt = 0.02;
+           // params
+           float distance_between_wp = 40;
+           int n_wp = 5;
+           int total_n_points = 50;
+           // acceleration
+           double acc = 1.2;
+           // number of previous points to keep
+           int n_prev_points = 20;
 
-           /////////////////////  Sensor Fusion
+
+
+           unsigned int prev_size = previous_path_x_.size();
+
+
+           // set the prev traj values to
+           // we do this to continue with the last path
+
+           std::vector<double> previous_path_x;
+           std::vector<double> previous_path_y;
+           int min = prev_size > n_prev_points ? n_prev_points : prev_size;
+           for(int i=0; i<min; ++i){
+             previous_path_x.push_back(previous_path_x_[i]);
+             previous_path_y.push_back(previous_path_y_[i]);
+           }
+           prev_size = previous_path_x.size();
            if (prev_size > 0){
-             car_s = end_path_s;
+             vector<double> sd = getFrenet(*(previous_path_x.end()-1), *(previous_path_y.end()-1), car_yaw, map_waypoints_x, map_waypoints_y);
+             car_s = sd[0];
            }
 
-           bool too_close = false;
 
-          bool right_lane_empty = true; // Initially defined these outside main()
-          bool left_lane_empty = true; // Initially flags were set to false, resulted in wrong logic
-          int line_with_space = 0;
 
-           for (int i = 0; i < sensor_fusion.size(); i++){
+
+           /////////////////////  Sensor Fusion
+           // all the lanes are empty till the moment we detect something
+           std::vector<bool> empty_lanes({true,true,true});
+
+           for (int i = 0; i < sensor_fusion.size(); ++i){
              // Review 2 - Lane number of vehicle
              float vehicle_d = sensor_fusion[i][6];
 
@@ -128,66 +156,49 @@ int main() {
              double vehicle_s = sensor_fusion[i][5];
 
              // predict the position of the car at the end of the prev trajectory
-             vehicle_s += ((double) prev_size * 0.02 * vehicle_speed);
+             vehicle_s += ((double) prev_size * dt * vehicle_speed);
 
              // car in the same line
              if (vehicle_lane == lane){
                // too close in the same line
-               if (vehicle_s > car_s && (vehicle_s - car_s) < 30){
-                 too_close = true;
+               if (vehicle_s > car_s && abs(vehicle_s - car_s) < 20){
+                 empty_lanes[lane] = false;
                }
-             }else if (vehicle_lane == (lane - 1)){
-               // Check if left lane empty or not
-               if ((vehicle_s < car_s && abs(vehicle_s - car_s) < 40) || (vehicle_s > car_s && abs(vehicle_s - car_s) < 20))
-               {
-                 left_lane_empty = false;
-               }
-             }else if (vehicle_lane == (lane + 1)){
-               // Check if right lane empty or not
-               if ((vehicle_s < car_s && abs(vehicle_s - car_s) < 40) || (vehicle_s > car_s && abs(vehicle_s - car_s) < 20)){
-                 right_lane_empty = false;
+             // different lane
+             }else{
+               if ((vehicle_s < car_s && abs(vehicle_s - car_s) < 10) || (vehicle_s > car_s && abs(vehicle_s - car_s) < 20)){
+                 empty_lanes[vehicle_lane] = false;
                }
              }
            }
 
-
-           // speed control
-           if (too_close)
+           /////////////////////  finite state machine???
+           if (!empty_lanes[lane])
            {
-             vel -= acc;
-
-             std::cout << "Is Left Lane empty: " << left_lane_empty << std::endl;
-             std::cout << "Is Right Lane empty: " << right_lane_empty << std::endl;
-
-             if (left_lane_empty && lane > 0)
-             {
-               std::cout << "Moving to Lane: " << lane-1 << std::endl;
+             if(lane > 0 && empty_lanes[lane-1]){
                lane -= 1;
-             }
-             else if (right_lane_empty && lane < 2)
-             {
-               std::cout << "Moving to Lane: " << lane+1 << std::endl;
+             }else if(lane < 2 && empty_lanes[lane+1]){
                lane += 1;
+             }else{
+               vel -= acc;
              }
-
-             std::cout << "In Lane: " << lane << std::endl;
-
-           }
-           else if (vel < 49)
-           {
-             vel += acc*1.5;
+           }else if(vel < max_speed){
+             vel += acc;
            }
 
 
-           // Trajectory definition
+
+
+
+
+           //////////////////////// Trajectory
            std::vector<double> ptsx;
            std::vector<double> ptsy;
            double ref_x = car_x;
            double ref_y = car_y;
-           double ref_yaw = deg2rad(car_yaw);
+           double ref_yaw = car_yaw;
 
-           if (prev_size < 2)
-           {
+           if (prev_size < 2){
              double prev_car_x = car_x - cos(ref_yaw);
              double prev_car_y = car_y - sin(ref_yaw);
 
@@ -195,9 +206,7 @@ int main() {
              ptsx.push_back(ref_x);
              ptsy.push_back(prev_car_y);
              ptsy.push_back(ref_y);
-           }
-           else
-           {
+           }else{
              ref_x = previous_path_x[prev_size - 1];
              ref_y = previous_path_y[prev_size - 1];
 
@@ -212,67 +221,59 @@ int main() {
            }
 
            // append n_wp new waypoints, equaly spaced
-           int n_wp = 3;
            for(int i=1; i<=n_wp; ++i){
-             std::vector<double> new_wp = getXY(car_s + i*30, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+             std::vector<double> new_wp = getXY(car_s + i*distance_between_wp, (0.5 + lane)*lane_width, map_waypoints_s, map_waypoints_x, map_waypoints_y);
              ptsx.push_back(new_wp[0]);
              ptsy.push_back(new_wp[1]);
            }
 
-           // Convert to Vehicle coordinates
-
-           for (int i=0; i < ptsx.size(); i++)
-           {
+           // to car coordinates
+           for (int i=0; i < ptsx.size(); ++i){
              double shift_x = ptsx[i] - ref_x;
              double shift_y = ptsy[i] - ref_y;
 
              ptsx[i] = (shift_x*cos(-ref_yaw)) - (shift_y*sin(-ref_yaw));
              ptsy[i] = (shift_x*sin(-ref_yaw)) + (shift_y*cos(-ref_yaw));
-
            }
 
-           // Create spline
-           //
-           tk::spline new_s;
-           new_s.set_points(ptsx, ptsy);
+           // spline
+           tk::spline spline;
+           spline.set_points(ptsx, ptsy);
 
-
-           // Add unused points
-
-           for (int i=0; i < previous_path_x.size(); i++)
-           {
+           // use the prev points
+           for (int i=0; i < previous_path_x.size(); ++i){
              next_x_vals.push_back(previous_path_x[i]);
              next_y_vals.push_back(previous_path_y[i]);
            }
 
            // calculate how we break up spline points so that we travel at our desired reference velocity
-           double target_x = 30.0;
-           double target_y = new_s(target_x);
+           double target_x = distance_between_wp;
+           double target_y = spline(target_x);
 
            double target_distance = sqrt((target_x * target_x) + (target_y * target_y));
 
            double x_add_on = 0;
-           double N = target_distance/(0.02 * vel/2.24); // 2.4 -> mps to mph
-           //cout << vel << endl;
-           // Generate points to total 50 waypoints
-           for(int i=1; i <= 50-previous_path_x.size(); i++)
-           {
+           double n_points_between_wp = target_distance/(dt * vel/2.24); // 2.24 -> mps to mph
 
-             double x_point = x_add_on + (target_x)/N;
-             double y_point = new_s(x_point);
+           // Generate points to total 50 points
+           for(int i=1; i <= total_n_points-previous_path_x.size(); ++i){
+
+             double x_point = x_add_on + (distance_between_wp)/n_points_between_wp;
+             double y_point = spline(x_point);
 
              x_add_on = x_point;
 
              double x_ref = x_point;
              double y_ref = y_point;
 
-             // Transform coordinate system
+             // to map coordinates
              x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
              y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
 
              x_point += ref_x;
              y_point += ref_y;
 
+             // save the points
              next_x_vals.push_back(x_point);
              next_y_vals.push_back(y_point);
            }
